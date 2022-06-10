b4106e78ba32d09b03ba67ddc06dbb6d2d3c64b6
kalanaweerakoon99 <weerakoonwakt.19@uom.lk>
2022-06-10 19:46:26 +0530

    after connecting front end and backend of appoinment

--- client/src/Pages/Appoinment/AddAppoinment.js
+++ client/src/Pages/Appoinment/AddAppoinment.js
@@ -1,51 +1,70 @@
-import React from "react";
+import {React,useState} from "react";
 import {Form,Button} from "react-bootstrap";
 import './appoinment.css'; 
+import {addAppoinment} from "../../utils/appoinment"
 
 function AddAppoinment(){
+
+  const [name,setName]= useState("")
+  const [numOfGuest,setnumOfGuest]= useState(0)
+  const [NIC,setNIC]= useState("")
+  const [date,setdate]= useState("")
+  const [mobileNo,setmobileNo]= useState("")
+  const [expDate,setexpDate]= useState("")
+
+  const submitAppoinment = async (event) =>{
+    event.preventDefault();
+    console.log({name,numOfGuest,NIC,date,mobileNo,expDate})
+    const res = await addAppoinment({
+      "appointmentStatus":"Active",
+      "appointmentName":"Jimmyyyyyyyyyyy",
+      "numOfGuest":9
+  })
+    alert(res)
+  }
     return(
 <>
 
-<Form>
+<Form onSubmit={submitAppoinment}>
 <h2>Add Appoinment</h2>
 <br/>
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label className="lablecss">Name</Form.Label>
-    <Form.Control type="text" placeholder="Name of the guest" className='inputcss'/>
+    <Form.Control type="text" placeholder="Name of the guest" className='inputcss' onChange={(e)=>setName(e.target.value)}/>
   </Form.Group>
   <br/><br/>
 
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label className="lablecss">No of Guests </Form.Label>
-    <Form.Control type="number" placeholder="How many visitors" className='inputcss' />
+    <Form.Control type="number" placeholder="How many visitors" className='inputcss' onChange={(e)=>setnumOfGuest(e.target.value)}/>
   </Form.Group>
   <br/><br/>
 
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label className="lablecss">NIC </Form.Label>
-    <Form.Control type="text" placeholder="NIC" className='inputcss' />
+    <Form.Control type="text" placeholder="NIC" className='inputcss' onChange={(e)=>setNIC(e.target.value)}/>
   </Form.Group>
   <br/><br/>
 
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label className="lablecss">Date</Form.Label>
-    <Form.Control type="date" placeholder="Date of visiting" className='inputcss' />
+    <Form.Control type="date" placeholder="Date of visiting" className='inputcss' onChange={(e)=>setdate(e.target.value)}/>
   </Form.Group>
   <br/><br/>
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label className="lablecss">Mobile No</Form.Label>
-    <Form.Control type="text" placeholder="Mobile number" className='inputcss'/>
+    <Form.Control type="text" placeholder="Mobile number" className='inputcss' onChange={(e)=>setmobileNo(e.target.value)}/>
   </Form.Group>
   <br/><br/>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label className="lablecss">Expiration Date</Form.Label>
-    <Form.Control type="date" placeholder="Deppature date" className='inputcss'/>
+    <Form.Control type="date" placeholder="Deppature date" className='inputcss' onChange={(e)=>setexpDate(e.target.value)}/>
   </Form.Group>
   <br/><br/><br/>
 
--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app =require("./app")
 const port = process.env.PORT;
 
 
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(4000,()=>{
+    console.log("Server is up on port 6000")
     
 })
