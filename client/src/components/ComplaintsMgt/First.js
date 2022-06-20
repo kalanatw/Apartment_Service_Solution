9c24231b7b87edc1a7eee9b91841c764bd51d65e
SilvaIK <silvadik.19@uom.lk>
2022-06-20 13:51:25 +0530

    admin web change1

--- client/src/components/ComplaintsMgt/First.js
+++ client/src/components/ComplaintsMgt/First.js
@@ -40,8 +40,8 @@ function ComplaintsTable() {
     await axios
       .get("http://localhost:8000/complaints")
       .then((res) => {
-        console.log(res.data);
-        setData(res.data);
+        console.log(res.data.data);
+        setData(res.datadata);
       })
       .catch((err) => {
         console.log(err);
